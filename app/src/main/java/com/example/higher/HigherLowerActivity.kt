b9779fd9cdd71e20fd2cf748340b7b3962114a47
idodela <i_delawar@hotmail.com>
2020-02-09 00:35:05 +0100

    Level 1 task 1 done

--- app/src/main/java/com/example/higher/HigherLowerActivity.kt
+++ app/src/main/java/com/example/higher/HigherLowerActivity.kt
@@ -1,9 +1,12 @@
 package com.example.higher
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_layer_lower.*
 
@@ -23,9 +26,9 @@ class HigherLowerActivity : AppCompatActivity() {
      */
     private fun initViews() {
         // set code to execute when button is tapped/clicked
-        higher.setOnClickListener {onHigherClick()}
-        lower.setOnClickListener{onLowerClick()}
-        equals.setOnClickListener{onEqualClick()}
+        higher.setOnClickListener { onHigherClick() }
+        lower.setOnClickListener { onLowerClick() }
+        equals.setOnClickListener { onEqualClick() }
 
         updateUI()
 
@@ -37,6 +40,28 @@ class HigherLowerActivity : AppCompatActivity() {
     private fun updateUI() {
         tvLastThrow.text = getString(R.string.lastThrow, lastThrow)
 
+
+        // set image into imageView dependent on the random number
+
+        if (currentThrow == 1) {
+            imageView.setImageResource(R.drawable.dice1)
+        } else if (currentThrow == 2) {
+            imageView.setImageResource(R.drawable.dice2)
+        } else if (currentThrow == 3) {
+            imageView.setImageResource(R.drawable.dice3)
+
+        } else if (currentThrow == 4) {
+            imageView.setImageResource(R.drawable.dice4)
+
+        } else if (currentThrow == 5) {
+            imageView.setImageResource(R.drawable.dice5)
+
+        } else if (currentThrow == 6) {
+            imageView.setImageResource(R.drawable.dice6)
+
+        }
+
+
     }
 
     /**
@@ -44,7 +69,7 @@ class HigherLowerActivity : AppCompatActivity() {
      * replaces the current dice with a new dice with a random number between 1 and 6 (6 included)
      */
 
-    private fun rollDice(){
+    private fun rollDice() {
         lastThrow = currentThrow
         currentThrow = (1..6).random()
         updateUI()
@@ -54,15 +79,15 @@ class HigherLowerActivity : AppCompatActivity() {
     /**
      * calls [rollDice]  and checks if the next throw is actually higher than the current dice.
      */
-    private fun onHigherClick(){
+    private fun onHigherClick() {
 
         rollDice()
 
         // checks if the nextThrow(when button is clicked nextThrow is actually currentThrow and
         // previous currentThrow is lastthrow) is higher than lastThrow )
 
-        if(currentThrow > lastThrow)
-          return  onAnswerCorrect()
+        if (currentThrow > lastThrow)
+            return onAnswerCorrect()
 
 
         onAnswerIncorrect()
@@ -73,7 +98,7 @@ class HigherLowerActivity : AppCompatActivity() {
      */
     private fun onLowerClick() {
         rollDice()
-        if(currentThrow < lastThrow)
+        if (currentThrow < lastThrow)
             return onAnswerCorrect()
 
 
@@ -86,7 +111,7 @@ class HigherLowerActivity : AppCompatActivity() {
      */
     private fun onEqualClick() {
         rollDice()
-        if(currentThrow == lastThrow)
+        if (currentThrow == lastThrow)
             return onAnswerCorrect()
 
         onAnswerIncorrect()
@@ -94,15 +119,16 @@ class HigherLowerActivity : AppCompatActivity() {
     }
 
 
-
     /**
      * Displays a successful Toast message.
      */
     private fun onAnswerCorrect() {
-        val text = "Guess correct!"
+        var text = "Guess correct!"
         val duration = Toast.LENGTH_SHORT
 
-        val toast = Toast.makeText(applicationContext, text, duration)
+        var toast = Toast.makeText(applicationContext, text, duration)
+        val txtToast : TextView = toast.view.findViewById(android.R.id.message)
+        txtToast.setTextColor(Color.BLACK)
         toast.setGravity(Gravity.TOP or Gravity.CENTER, 0, 0)
 
         toast.show()
@@ -116,6 +142,8 @@ class HigherLowerActivity : AppCompatActivity() {
         val duration = Toast.LENGTH_SHORT
 
         val toast = Toast.makeText(applicationContext, text, duration)
+        val txtToast : TextView = toast.view.findViewById(android.R.id.message)
+        txtToast.setTextColor(Color.BLACK)
         toast.setGravity(Gravity.TOP or Gravity.CENTER, 0, 0)
         toast.show()
     }
